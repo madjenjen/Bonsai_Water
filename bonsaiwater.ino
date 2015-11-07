260bf75decd93c9cb6849cca6be76d73dd19dc03
madjenjen <jmad2319@gmail.com>
2015-11-07 14:55:06 -0500

    Initial commit including code for Arduino.

---
+++ bonsaiwater.ino
@@ -0,0 +1,31 @@
+int pump = A0;
+int light = 13;
+int sensor = A1;
+
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  pinMode(pump, OUTPUT);
+  pinMode(light, OUTPUT);
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(sensor); 
+     
+  if (sensorValue >=600){
+    Serial.println(sensorValue);
+    digitalWrite(pump, HIGH);
+    digitalWrite(light, HIGH);
+    Serial.print("Set pump: ");
+    Serial.println(digitalRead(pump));
+  }
+  else{
+    Serial.println(sensorValue);
+    digitalWrite(pump, LOW);
+    digitalWrite(light, LOW);
+  }
+  delay(1000);
+}
+
